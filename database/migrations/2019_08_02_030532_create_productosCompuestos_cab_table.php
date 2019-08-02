5791ac3f5512742312926b5e4c797fbcd7e36b7d
Victor Veliz <ing-vj@outlook.com>
2019-08-01 23:09:24 -0400

    Agregado productoCompuesto_cab

---
+++ database/migrations/2019_08_02_030532_create_productosCompuestos_cab_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductosCompuestosCabTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productosCompuestos_cab', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('compuesto');
+            $table->date('fecha');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productosCompuestos_cab');
+    }
+}
