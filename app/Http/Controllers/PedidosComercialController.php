f8a6632e64d80461b131ae1977400865386e1f95
Victor Veliz <ing-vj@outlook.com>
2019-09-16 09:30:42 -0400

    Vista de Pedidos Comerciales.

--- app/Http/Controllers/PedidosComercialController.php
+++ app/Http/Controllers/PedidosComercialController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\CatDiasSemana;
+use App\Cliente;
 use App\Cultivo;
 use Illuminate\Http\Request;
 
@@ -10,7 +11,7 @@ class PedidosComercialController extends Controller
 {
     public function index(Request $request)
     {
-        $fecha_act          = (is_null($request->fecha_act)) ? date('Y-m-d') : date("Y-m-d", strtotime($request->fecha_act));
+        $fecha_act = (is_null($request->fecha_act)) ? date('Y-m-d') : date("Y-m-d", strtotime($request->fecha_act));
         if (!is_null($request->semana_act)) {
             $data['semana_act'] = intval($request->semana_act);
         } else {
@@ -21,9 +22,11 @@ class PedidosComercialController extends Controller
         $data['semana_fin'] = 50;
 
         $cultivos = Cultivo::all();
+        $clientes = Cliente::all();
 
         $data['fecha_act'] = $fecha_act;
         $data["cultivos"]  = $cultivos;
+        $data['clientes']  = $clientes;
 
         return view("comercial.pedidos-comercial.index")->with($data);
     }
--- resources/views/comercial/pedidos-comercial/index.blade.php
+++ resources/views/comercial/pedidos-comercial/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('main-content')
     <div class="breadcrumb">
-            <h1>Comercial</h1>
+        <h1>Comercial</h1>
         <ul>
             {{-- <li><a href="">UI Kits</a></li> --}}
             <li>Pedidos Comerciales</li>
@@ -18,26 +18,232 @@
                 <div class="card-body">
                     <div class="card-title mb-3">Pedidos Comerciales</div>
 
-                    <form action="/prevision" method="GET" id="form_semana_act">
+                    <!-- Modal Pedido-->
+                    <div class="modal fade" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
+                         aria-hidden="true" id="modal-pedido">
+                        <div class="modal-dialog modal-lg">
+                            <div class="modal-content">
+                                <form action="/comercial/pedidos-comercial" method="POST" id="pedido_form">
+                                    {{ csrf_field() }}
+                                    <input type="hidden" name="_method" id="pedido_method" value="">
+                                    <input type="hidden" name="id" id="pedido_id">
+
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="modal-pedido-title"></h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <div class="row">
+                                            <div class="col-md-4 mb-3">
+                                                <label for="nro_orden">Nº Orden</label>
+                                                <input type="text" name="nro_orden" id="nro_orden" class="form-control">
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-md-4 mb-3">
+                                                <label for="cliente">Cliente</label>
+                                                <select class="form-control chosen" id="cliente" name="producto">
+                                                    @foreach ($clientes as $cliente)
+                                                        <option value="{{ $cliente->id }}">{{ $cliente->razon_social }}</option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                            <div class="col-md-4 mb-3">
+                                                <label for="nro_lote_pedido">Destino Comercial</label>
+                                                <input type="text" name="destino_comercial" id="destino_comercial"
+                                                       class="form-control" value="">
+                                            </div>
+                                            <div class="col-md-4 mb-3">
+                                                <label for="producto">Producto</label>
+                                                <select class="form-control chosen" id="producto" name="producto">
+                                                    @foreach ($cultivos as $cultivo)
+                                                        <option value="{{ $cultivo->id }}">{{ $cultivo->cultivo }}</option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-md-3 mb-3">
+                                                <label class="checkbox checkbox-success">
+                                                    <input type="checkbox" name="dia[]" value="1" class="dias">
+                                                    <span>Miércoles</span>
+                                                    <span class="checkmark"></span>
+                                                </label>
+                                            </div>
+                                            <div class="col-md-3 mb-3">
+                                                <label class="checkbox checkbox-success">
+                                                    <input type="checkbox" name="dia[]" value="2" class="dias">
+                                                    <span>Jueves</span>
+                                                    <span class="checkmark"></span>
+                                                </label>
+                                            </div>
+                                            <div class="col-md-3 mb-3">
+                                                <label class="checkbox checkbox-success">
+                                                    <input type="checkbox" name="dia[]" value="3" class="dias">
+                                                    <span>Viernes</span>
+                                                    <span class="checkmark"></span>
+                                                </label>
+                                            </div>
+                                            <div class="col-md-3 mb-3">
+                                                <label class="checkbox checkbox-success">
+                                                    <input type="checkbox" name="dia[]" value="4" class="dias">
+                                                    <span>Sábado</span>
+                                                    <span class="checkmark"></span>
+                                                </label>
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-md-3 mb-3">
+                                                <label class="checkbox checkbox-success">
+                                                    <input type="checkbox" name="dia[]" value="5" class="dias">
+                                                    <span>Domingo</span>
+                                                    <span class="checkmark"></span>
+                                                </label>
+                                            </div>
+                                            <div class="col-md-3 mb-3">
+                                                <label class="checkbox checkbox-success">
+                                                    <input type="checkbox" name="dia[]" value="6" class="dias">
+                                                    <span>Lunes</span>
+                                                    <span class="checkmark"></span>
+                                                </label>
+                                            </div>
+                                            <div class="col-md-3 mb-3">
+                                                <label class="checkbox checkbox-success">
+                                                    <input type="checkbox" name="dia[]" value="7" class="dias">
+                                                    <span>Martes</span>
+                                                    <span class="checkmark"></span>
+                                                </label>
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-md-4 mb-3">
+                                                <label for="formato">Formato</label>
+                                                <input type="text" class="form-control" name="formato" id="formato">
+                                            </div>
+                                            <div class="col-md-4 mb-3">
+                                                <label for="etiqueta">Etiqueta</label>
+                                                <input type="text" class="form-control" name="etiqueta" id="etiqueta">
+                                            </div>
+                                            <div class="col-md-4 mb-3">
+                                                <label for="transporte">Transporte</label>
+                                                <input type="text" name="transporte" id="transporte"
+                                                       class="form-control">
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-md-4 mb-3">
+                                                <label for="precio">Precio Kg</label>
+                                                <input type="text" name="precio" id="precio" class="form-control">
+                                            </div>
+                                            <div class="col-md-4 mb-3">
+                                                <label for="kilos">Kilos</label>
+                                                <input type="number" class="form-control" name="kilos" id="kilos"
+                                                       step="0.01" min="0.00" placeholder="0.00">
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-md-12 mb-3">
+                                                <label for="comentario">Comentario</label>
+                                                <textarea class="form-control" name="comentario" id="comentario"
+                                                          rows="2"></textarea>
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-md-4 mb-3">
+                                                <label for="estado">Estado</label>
+                                            </div>
+                                        </div>
+
+                                    </div>
+
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">
+                                            Cerrar
+                                        </button>
+                                        <button type="submit" class="btn btn-primary">Guardar</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- Fin modal Pedido --}}
+
+                    <form action="/comercial/pedidos-comercial" method="GET" id="form_fecha_act">
                         <div class="row">
-                            <div class="col-md-3 mb-3">
+                            <div class="col-md-3 form-group mb-3">
                                 <label>Semana</label>
-                                <select class="form-control" name="semana_act" id="semana_act">
-                                    @for($i = $semana_ini; $i <= $semana_fin; $i++)
-                                        <option
-                                                {{ ($i == $semana_act) ? 'selected' : '' }} value="{{ $i }}">
-                                            {{ $i }}
-                                        </option>
-                                    @endfor
-                                </select>
+                                <input type="text" readonly value="{{ $semana_act }}" class="form-control">
                             </div>
                             <div class="col-md-3 form-group mb-3">
                                 <label>Fecha</label>
                                 <input type="date" name="fecha_act" id="fecha_act" class="form-control"
                                        value="{{ $fecha_act }}">
                             </div>
+
+                            <div class="col-md-6  text-right form-group mb-3">
+                                <br>
+                                <button class="btn btn-outline-primary" type="button" id="btnAddPedido">Añadir Pedido
+                                </button>
+                            </div>
                         </div>
                     </form>
+
+                    <div class="row">
+                        <div class="col-md-12 mb-3">
+                            <label>Cultivos</label>
+
+                            <ul class="nav nav-pills" id="myPillTab" role="tablist">
+                                @foreach ($cultivos as $c => $cultivo)
+                                    <li class="nav-item">
+                                        <a class="nav-link {{ ($c==0) ? 'active show' : '' }}" id="home-icon-pill"
+                                           data-toggle="pill"
+                                           href="#cultivo_{{ $cultivo->id }}_pill" role="tab"
+                                           aria-controls="cultivo_{{ $cultivo->id }}_pill"
+                                           aria-selected="{{ ($c==0) ? 'true' : 'false' }}">{{ $cultivo->cultivo }}</a>
+                                    </li>
+                                @endforeach
+                            </ul>
+                            <div class="tab-content" id="myPillTabContent">
+                                @foreach ($cultivos as $c => $cultivo)
+                                    <div class="tab-pane fade {{ ($c==0) ? 'active show' : '' }}"
+                                         id="cultivo_{{ $cultivo->id }}_pill" role="tabpanel"
+                                         aria-labelledby="cultivo_{{ $cultivo->id }}_pill">
+                                        <div class="table-responsive">
+                                            <table class="table table_pedidos">
+                                                <thead>
+                                                <tr>
+                                                    <th>ID</th>
+                                                    <th scope="col">Nº Orden</th>
+                                                    <th scope="col">Fecha</th>
+                                                    <th scope="col">Cliente</th>
+                                                    <th scope="col">Destino</th>
+                                                    <th>Formato</th>
+                                                    <th>Etiqueta</th>
+                                                    <th>Transporte</th>
+                                                    <th>Precio €/Kg</th>
+                                                    <th>Observación</th>
+                                                    <th>Estado</th>
+                                                    <th scope="col">Acciones</th>
+                                                </tr>
+                                                </thead>
+                                                <tbody>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -70,5 +276,59 @@
                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
             }
         });
+    </script>
+
+    <script>
+        $(function () {
+            $("#fecha_act").change(function () {
+                $("#form_fecha_act").submit();
+            });
+        })
+    </script>
+
+    <script>
+        $(document).ready(function() {
+            // Configuracion de Datatable
+            $('.table_pedidos').DataTable({
+                language: {
+                    url: "{{ asset('assets/Spanish.json')}}"
+                },
+                columnDefs: [
+                    {targets: [0], visible: false},
+                ],
+                responsive: true
+            });
+
+            $('#clientes_table .delete').on('click', function () {
+                var tr = $(this).closest('tr');
+                var row = table_clientes.row(tr).data();
+
+                swal({
+                    title: 'Confirmar Proceso',
+                    text: "Confirme eliminar el registro seleccionado",
+                    type: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#0CC27E',
+                    cancelButtonColor: '#FF586B',
+                    confirmButtonText: 'Confirmar',
+                    cancelButtonText: 'Cancelar',
+                    confirmButtonClass: 'btn btn-success mr-5',
+                    cancelButtonClass: 'btn btn-danger',
+                    buttonsStyling: false
+                }).then(function () {
+                    window.location.href = "{{ url('comercial/clientes/delete') }}" + "/" + row[0]
+                })
+            });
+
+            $("#btnAddPedido").click(function (e) {
+                //limpiarCamposProveedor();
+                $("#modal-pedido-title").html("Nuevo Pedido");
+                $("#modal-pedido").modal('show');
+            })
+        });
+
+        function limpiarCamposProveedor() {
+            $('#cif, #razon_social').val(null);
+        }
     </script>
 @endsection
